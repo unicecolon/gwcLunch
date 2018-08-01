431015e033de1320437b91371851e19ab38f6c87
unicecolon <unicecolon@gmail.com>
2018-08-01 15:51:56 -0400

    Lunch Project

---
+++ lunch.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event))
+{
+    var sand1 = document.getElementById('Image')
+    console.log(sand1)
+    sand1.addEventsListener('click', function(e))
+    {
+        document.getElementById('Image').scr=//name of image saved
+    }
+}
