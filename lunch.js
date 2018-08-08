49b3d5013818ade51ce53dd9dc957edbaa96b03b
unicecolon <unicecolon@gmail.com>
2018-08-08 09:17:25 -0400

    js

--- lunch.js
+++ lunch.js
@@ -1,9 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event))
+document.addEventListener('DOMContentLoaded', function loaded (event)
 {
-    var sand1 = document.getElementById('Image')
-    console.log(sand1)
-    sand1.addEventsListener('click', function(e))
+    console.log("I'm Loaded!");
+    var image = document.getElementById('Image');
+    image.addEventListener('click', function imageClick (e)
     {
-        document.getElementById('Image').scr=//name of image saved
-    }
-}
+        console.log("I just clicked on the sandwich iamge.")
+        console.log(image.scr)
+        if (document.getElementById("Image").src == "https://media-cdn.tripadvisor.com/media/photo-s/09/6c/42/7a/homemade-soups-and-sandwiches.jpg") 
+        {
+            document.getElementById("Image").src = "https://cms.splendidtable.org/sites/default/files/styles/w2000/public/SaladSandwich_DAY3.AV_..0053.jpg?itok=SXzKMGyz";
+        }
+        else 
+        {
+            document.getElementById("Image").src = "https://media-cdn.tripadvisor.com/media/photo-s/09/6c/42/7a/homemade-soups-and-sandwiches.jpg";
+        }
+    })
+})
